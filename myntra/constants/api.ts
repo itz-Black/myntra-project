8017cda4c607dd04c2230f2381b8d26f348d3e16
Rahul <raghul2201@gmail.com>
2026-03-20 21:56:06 +0530

    Change API URLs for LOCAL_API and REMOTE_API
    Updated API endpoints to use the new Render backend URL.

--- myntra/constants/api.ts
+++ myntra/constants/api.ts
@@ -5,10 +5,7 @@
  * - REMOTE_API → Deployed Render backend (products, bag, wishlist, orders)
  */
 
-<<<<<<< HEAD
+
 export const LOCAL_API = "https://api-myntra-backend.onrender.com";
 export const REMOTE_API = "https://api-myntra-backend.onrender.com";
-=======
-export const LOCAL_API = "https://myntra-project-6nci.onrender.com";
-export const REMOTE_API = "https://myntra-project-6nci.onrender.com";
->>>>>>> origin/main
+
